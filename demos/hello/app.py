e32033360730583c18a381fd474ff237afc87a53
h_mayuyang <h_mayuyang@qq.com>
2020-06-04 09:56:08 +0800

    learn from liao

--- demos/hello/app.py
+++ demos/hello/app.py
@@ -6,15 +6,17 @@
     :license: MIT, see LICENSE for more details.
 """
 import click,os
-from flask import Flask, request, redirect, url_for, make_response,jsonify,session
+from flask import Flask, request, redirect, url_for, make_response, jsonify, session, abort
+from urllib.parse import urlparse,urljoin
+
 
 app = Flask(__name__)
 
 
 # the minimal Flask application
-@app.route('/')
-def index():
-    return '<h1>Hello, World!</h1>'
+# @app.route('/')
+# def index():
+#     return '<h1>Hello, World!</h1>'
 
 
 # bind multiple URL for one view function
@@ -74,9 +76,10 @@ def foo():
 
 @app.route('/set/<name>')
 def foo2(name):
-    response = make_response(redirect(url_for('hello_again')))
+    response = make_response(redirect(url_for('hello2')))
     response.set_cookie('name',name)
     return  response
+
 
 
 @app.route('/')
@@ -84,11 +87,53 @@ def foo2(name):
 def hello2():
     name = request.args.get('name')
     if name is None:
-        name = request.args.get('name','wuhan')
-    return '<h1>Hello,%s</h1>'%name
+        name = request.cookies.get('name','wuhan')
+        response = '<h1>Hello,%s</h1>'%name
+    if 'logged_in' in session:
+        response +='[Authenticated]'
+    else:
+        response += '[Not Authenticated]'
+    return response
 
 app.secret_key = os.getenv('SECRET_KEY','DRMHZE6EPDSJKJKHJSD89_81bj-na')
 @app.route('/login')
 def login():
     session['logged_in'] = True
-    return redirect(url_for('hello_again'))
+    return redirect(url_for('hello2'))
+
+
+@app.route('/admin')
+def admin():
+    if 'logged_in' not in session:
+        abort(403)
+    return 'Welcome to admin page'
+
+
+@app.route('/logout')
+def logout():
+    if 'logged_id' in session:
+        session['logged_in'] = False
+    return redirect(url_for('hello2'))
+
+
+def redirect_back(default = 'hello2',**kwargs):
+    for target in request.args.get('next'),request.referrer:
+        if target:
+            return redirect(target)
+    return redirect(default,**kwargs)
+
+
+@app.route('/do_something')
+def do_something():
+    # return redirect(request.referrer or url_for('hello2'))
+    # print(request.args.get('next'))
+    # return redirect(request.args.get('next',url_for('hello2')))
+    return redirect_back()
+
+@app.route('/foo1')
+def foo1():
+    return '<h1>Foo page<a href="%s">back</a></h1>'%url_for('do_something',next = request.full_path)
+
+@app.route('/bar')
+def bar():
+    return '<h1>Bar page<a href="%s">bar</a></h1>'%url_for('do_something',next = request.full_path)
